268c12a0717fd7d202800ba1ae3bb2365f4a6531
johess123 <s109213067@mail1.ncnu.edu.tw>
2025-05-26 01:50:04 +0800

    fix_change_health_check_method_to_head

--- src/router/healthcheck.py
+++ src/router/healthcheck.py
@@ -1,11 +1,7 @@
-from fastapi import APIRouter
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Response
 
 router = APIRouter()
 
-@router.get("/health_check")
-async def health_check():
-    return JSONResponse(
-        content={"message": "success"},
-        status_code=200
-    )
+@router.head("/health_check")
+async def health_check_head():
+    return Response(status_code=200)
